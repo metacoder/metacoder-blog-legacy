470bb676476d50c93a3b18cf9ae7122a28fe8934
Felix Becker <git@felixbecker.name>
2012-06-08 16:09:38 +0200

    schema exporter um magic spring power erweitert, yeah.

--- src/main/java/de/metacoder/blog/persistence/SchemaExporter.java
+++ src/main/java/de/metacoder/blog/persistence/SchemaExporter.java
@@ -1,22 +1,44 @@
 package de.metacoder.blog.persistence;
 
-import org.hibernate.cfg.Configuration;
-import org.hibernate.cfg.Environment;
-import org.hibernate.tool.hbm2ddl.SchemaExport;
+import java.util.ArrayList;
+import java.util.Collection;
 
-import de.metacoder.blog.persistence.entities.BlogEntry;
-import de.metacoder.blog.persistence.entities.User;
+import javax.persistence.Entity;
+
+import org.hibernate.cfg.Configuration;
+import org.hibernate.tool.hbm2ddl.SchemaExport;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.context.annotation.ClassPathScanningCandidateComponentProvider;
+import org.springframework.core.type.filter.AnnotationTypeFilter;
 
 public class SchemaExporter {
-	public static void main(String... args){
+	
+	public static void main(String... args) {
 		Configuration configuration = new Configuration();
-		// doesn't work, dunno why yet
-//		configuration.addPackage("de.metacoder.blog.persistence.entities");
 		configuration.setProperty("hibernate.dialect", "org.hibernate.dialect.H2Dialect");
-		configuration.addAnnotatedClass(BlogEntry.class);
-		configuration.addAnnotatedClass(User.class);
+		
+		for(Class<?> clazz : getEntitiesFromPackage("de.metacoder.blog.persistence.entities")){
+			System.out.println("adding " + clazz);
+			configuration.addAnnotatedClass(clazz);
+		}
+		
 		SchemaExport schemaExport = new SchemaExport(configuration);
 		schemaExport.setOutputFile("src/main/resources/metacoder-create-h2.sql");
 		schemaExport.create(true, false);
+	}
+
+	protected static Collection<Class<?>> getEntitiesFromPackage(String packageName) {
+		Collection<Class<?>> foundEntities = new ArrayList<Class<?>>();
+		ClassPathScanningCandidateComponentProvider pathScanner = new ClassPathScanningCandidateComponentProvider(false);
+		pathScanner.addIncludeFilter(new AnnotationTypeFilter(Entity.class));
+		for(BeanDefinition beanDefinition : pathScanner.findCandidateComponents(packageName)){
+			try {
+				foundEntities.add(Class.forName(beanDefinition.getBeanClassName()));
+			} catch (ClassNotFoundException e) { 
+				// shoud never happen here so wrap it in a runtime exception to avoid poisoning the methods signature here
+				throw new RuntimeException(e);
+			}
+		}
+		return foundEntities;
 	}
 }
