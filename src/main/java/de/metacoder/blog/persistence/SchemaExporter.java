aec68c09dd07151d03e6b94ac9dc1565427fddc3
Felix Becker <git@felixbecker.name>
2012-06-08 15:01:43 +0200

    hibernate schema exporter hinzugefügt (erster test), h2 schema ddl generiert

---
+++ src/main/java/de/metacoder/blog/persistence/SchemaExporter.java
@@ -0,0 +1,21 @@
+package de.metacoder.blog.persistence;
+
+import org.hibernate.cfg.Configuration;
+import org.hibernate.tool.hbm2ddl.SchemaExport;
+
+import de.metacoder.blog.persistence.entities.BlogEntry;
+import de.metacoder.blog.persistence.entities.User;
+
+public class SchemaExporter {
+	public static void main(String... args){
+		Configuration configuration = new Configuration();
+		// doesn't work, dunno why yet
+//		configuration.addPackage("de.metacoder.blog.persistence.entities");
+		configuration.setProperty("hibernate.dialect", "org.hibernate.dialect.H2Dialect");
+		configuration.addAnnotatedClass(BlogEntry.class);
+		configuration.addAnnotatedClass(User.class);
+		SchemaExport schemaExport = new SchemaExport(configuration);
+		schemaExport.setOutputFile("src/main/resources/metacoder-create-h2.sql");
+		schemaExport.create(true, false);
+	}
+}
