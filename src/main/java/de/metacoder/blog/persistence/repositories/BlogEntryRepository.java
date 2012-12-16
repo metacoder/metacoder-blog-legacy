31468d04441d56643b3cd2df3968cd5c98ad3403
Felix Becker <git@felixbecker.name>
2012-12-16 06:35:43 +0100

    import clean up

--- src/main/java/de/metacoder/blog/persistence/repositories/BlogEntryRepository.java
+++ src/main/java/de/metacoder/blog/persistence/repositories/BlogEntryRepository.java
@@ -2,7 +2,6 @@ package de.metacoder.blog.persistence.repositories;
 
 import java.util.List;
 
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
