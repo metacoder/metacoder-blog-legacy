d8e0cbdce8d471ff6b98a30d5b6ff0d5400ff39f
Felix Becker <git@felixbecker.name>
2013-06-11 00:39:07 +0200

    angular js hinzugefügt

--- src/main/java/de/metacoder/blog/components/AdminLayout.java
+++ src/main/java/de/metacoder/blog/components/AdminLayout.java
@@ -1,6 +1,7 @@
 package de.metacoder.blog.components;
 
 import org.apache.tapestry5.BindingConstants;
+import org.apache.tapestry5.annotations.Import;
 import org.apache.tapestry5.annotations.Parameter;
 import org.apache.tapestry5.annotations.Property;
 
@@ -11,6 +12,10 @@ import org.apache.tapestry5.annotations.Property;
  * @author Felix Becker <a href="mailto:becker@jubeco.de>becker@jubeco.de</a>
  */
 
+@Import(
+        library ={
+                "context:scripts/angularjs/angular.min.js"
+        })
 public class AdminLayout {
 	
 	@Property
