8ee4513d715c4cec44c8bb4f6579688f93ab4f36
Felix Becker <git@felixbecker.name>
2013-07-20 23:10:36 +0200

    aaah

--- src/main/java/de/metacoder/blog/AprilFirstFilter.java
+++ src/main/java/de/metacoder/blog/AprilFirstFilter.java
@@ -20,7 +20,7 @@ public class AprilFirstFilter implements Filter {
 
 	@Override
 	public void doFilter(ServletRequest arg0, ServletResponse arg1, FilterChain arg2) throws IOException, ServletException {
-		if(isAprilFirst()){
+		if(isAprilFirst() && false){
 			arg1.getOutputStream().println("<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Transitional//EN\" \"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd\">" +
 											"<html><head><title>Metacoder.de</title></head><body style=\"background-color:#393058; background-image:url('http://www.afriserver.de/deadjim/deadjim.png'); background-repeat:no-repeat; background-position:center; background-attachment:fixed;\"></body></html>");
 		} else {
